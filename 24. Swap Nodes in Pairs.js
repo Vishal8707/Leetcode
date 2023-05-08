5c1ca81fb8040d5235b9774d7a6fafa533dbb292
Vishal8707 <360vishalsharma.mech@gmail.com>
2023-05-08 12:09:52 +0530

    Swap Nodes in Pairs

---
+++ 24. Swap Nodes in Pairs.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if(head==null || head.next==null) return head
+    let dummy = new ListNode(0)
+    dummy.next = head
+    let prev = dummy
+    let curr = head
+    while(curr!=null && curr.next!=null) {
+        let next = curr.next
+        curr.next = next.next
+        next.next = curr
+        prev.next = next
+        prev = curr
+        curr = curr.next
+    }
+    return dummy.next
+};
